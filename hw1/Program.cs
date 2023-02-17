f5485c12e58c09054f8752c80c58195b8556ba53
trynkecaas <122730663+trynkecaas@users.noreply.github.com>
2023-02-18 01:02:54 +0300

    hw1

---
+++ hw1/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = -8;
+int b = -5;
+int max = 0;
+int min = 0;
+
+if (a>b)
+{
+    max = a; min = b;
+    Console.Write("max=");
+    Console.WriteLine(max);
+    Console.Write("min=");
+    Console.WriteLine(min);
+}
+else
+{
+    max = b; min = a;
+    Console.Write("max=");
+    Console.WriteLine(max);
+    Console.Write("min=");
+    Console.WriteLine(min);
+}
