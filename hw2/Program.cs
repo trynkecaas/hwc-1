82b621c9ffa1ac028f44759a190c847265ffce3e
trynkecaas <122730663+trynkecaas@users.noreply.github.com>
2023-02-18 01:24:53 +0300

    hw2

---
+++ hw2/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberA = new Random().Next(1,1000); 
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,1000);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1,1000); 
+Console.WriteLine(numberC);
+int max = numberA ;
+
+if (numberA > max ) max = numberA;
+if (numberB > max ) max = numberB;
+if (numberC > max ) max = numberC;
+Console.Write("max = ");
+Console.WriteLine(max);
