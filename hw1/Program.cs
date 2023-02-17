8d6c3713bfc64a336314d1337c6787464af44c2d
trynkecaas <122730663+trynkecaas@users.noreply.github.com>
2023-02-18 02:11:05 +0300

    hw3

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,7 +1,12 @@
-﻿int a = -8;
-int b = -5;
-int max = 0;
-int min = 0;
+﻿double a; double b; double c;
+Console.WriteLine("a =");
+a = inputValue();
+Console.WriteLine("b =");
+b = inputValue();
+Console.WriteLine("c =");
+c = inputValue();
+double max = 0;
+double min = 0;
 
 if (a>b)
 {
---
+++ hw3/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 5 ;
+int os = a % 2; 
+if (os == 0)
+{
+  Console.WriteLine(a);
+   Console.Write("да");
+}
+else
+{
+  Console.WriteLine(a);
+  Console.WriteLine("нет");
+}
+
