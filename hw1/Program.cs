c7e71b4619900dbdecec8cc71ff09f3ad42a32cc
trynkecaas <122730663+trynkecaas@users.noreply.github.com>
2023-02-18 02:53:59 +0300

    hw1

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,12 +1,7 @@
-﻿double a; double b; double c;
-Console.WriteLine("a =");
-a = inputValue();
-Console.WriteLine("b =");
-b = inputValue();
-Console.WriteLine("c =");
-c = inputValue();
-double max = 0;
-double min = 0;
+﻿int a =-3; 
+int b =-6;
+int max = 0;
+int min = 0;
 
 if (a>b)
 {
